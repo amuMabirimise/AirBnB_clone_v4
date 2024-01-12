1780f584d14e331c9393548153ad6a685939c7a9
root <amuMabirimise>
2024-01-12 02:12:08 -0800

    Select some Amenities to be comfortable!

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
-""" Starting a Flask Web Application """
+""" Starting a Flash Web Application """
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from os import environ
 from flask import Flask, render_template
 import uuid
-
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
 
 @app.teardown_appcontext
 def close_db(error):
     """ Removing the current SQLAlchemy Session """
     storage.close()
 
+
 @app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
-    """ HBNB is looking alive! """
-    states = sorted(storage.all(State).values(), key=lambda k: k.name)
-    amenities = sorted(storage.all(Amenity).values(), key=lambda k: k.name)
-    places = sorted(storage.all(Place).values(), key=lambda k: k.name)
+    """ HBNB is lookinf alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
 
     return render_template('1-hbnb.html',
-                           states=states,
+                           states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
 
-if __name__ == "__main__":
-    """ Main of the Function """
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == "__main__":
+    """ Main of a Function """
+    app.run(host='0.0.0.0', port=5000)
